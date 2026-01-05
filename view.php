eb334adf4648e55bd61b82793515c16d15b9688b
weir <wei080781@gmail.com1>
2026-01-05 16:36:37 +0800

    新增switchcase

--- view.php
+++ view.php
@@ -38,12 +38,12 @@ switch($_GET['do']){
 <form action="?" method="post">
     <table>
         <tr>
-            <td></td>
-            <td></td>
+            <td>標題區圖片</td>
+            <td><input type="file" name="title_img" id=""></td>
         </tr>
         <tr>
-            <td></td>
-            <td></td>
+            <td>標題區替代文字</td>
+            <td><input type="text" name="alt" id=""></td>
         </tr>
     </table>
 </form>
