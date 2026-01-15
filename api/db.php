f21fa6caf59b7fc2bf5c8218583ae8492f24fd72
weir <wei080781@gmail.com1>
2026-01-15 09:16:08 +0800

    修改db檔

--- api/db.php
+++ api/db.php
@@ -9,130 +9,162 @@ session_start();
  * 
  */
 
-class DB
-{
-    private $dsn = "mysql:host=localhost;dbname=db01;charset=utf8";
+Class DB{
+    private $dsn="mysql:host=localhost;dbname=db01;charset=utf8";
     private $table;
     private $pdo;
-
-    public function __construct($table)
-    {
-        $this->table = $table;
-        $this->pdo = new PDO($this->dsn, 'root', '');
+                                
+    public function __construct($table){
+        $this->table=$table;
+        $this->pdo=new PDO($this->dsn,'root','');
     }
 
-    public function all(...$arg)
-    {
-        $sql = "select * from `$this->table` ";
-        if (isset($arg[0])) {
-            if (is_array($arg[0])) {
-                $tmp = $this->arrayToSql($arg[0]);
-                $sql .= " where " . implode(" && ", $tmp);
-            } else {
-                $sql .= $arg[0];
+    public function all(...$arg){
+
+        $sql="select * from `$this->table` ";
+        
+            if(isset($arg[0])){
+                if(is_array($arg[0])){
+                    //多條件
+                    $tmp=$this->arrayToSql($arg[0]);
+                    $sql .= " where " . implode(" && ",$tmp);
+                }else{
+                    //單條件
+                    $sql .=$arg[0];
+                }
             }
-        }
-        if (isset($arg[1])) {
-            $sql .= $arg[1];
-        }
+
+            if(isset($arg[1])){
+                $sql .=$arg[1];
+            }
+        // echo $sql;
         return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    function find($id)
-    {
-        $sql = "select * from `$this->table`";
-        if (is_array($id)) {
-            $tmp = $this->arrayToSql($id);
-            $sql .= " where " . implode(" && ", $tmp);
-        } else {
-
-            $sql .= " where `id` ='$id' ";
-        }
+    function find($id){
+        $sql="select * from `$this->table` ";
+                if(is_array($id)){
+                    //多條件
+                    $tmp=$this->arrayToSql($id);
+                    $sql .= " where " . implode(" && ",$tmp);
+                }else{
+                    //單條件
+                    $sql .= " where `id`='$id' ";
+                }
+          
+        // echo $sql;
         return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
     }
 
-    function save($array)
-    {
-        if (isset($array['id'])) {
+    function save($array){
+        if(isset($array['id'])){
             $this->update($array);
-        } else {
+        }else{
             $this->insert($array);
         }
+
     }
 
-    function count(...$arg)
-    {
-        $sql = "select count(*) from `$this->table` ";
-
-        if (isset($arg[0])) {
-            if (is_array($arg[0])) {
-                $tmp = $this->arrayToSql($arg[0]);
-                $sql .= " where " . implode(" && ", $tmp);
-            } else {
-                $sql .= $arg[0];
+    //計算符合條件的資料筆數
+    function count(...$arg){
+        $sql="select count(*) from `$this->table` ";
+        
+            if(isset($arg[0])){
+                if(is_array($arg[0])){
+                    //多條件
+                    $tmp=$this->arrayToSql($arg[0]);
+                    $sql .= " where " . implode(" && ",$tmp);
+                }else{
+                    //單條件
+                    $sql .=$arg[0];
+                }
             }
-        }
 
-        if (isset($arg[1])) {
-            $sql .= $arg[1];
-        }
+            if(isset($arg[1])){
+                $sql .=$arg[1];
+            }
+        // echo $sql;
         return $this->pdo->query($sql)->fetchColumn();
     }
 
-    function update($array)
-    {
-        $sql = "UPDATE $this->table ";
-        $tmp = $this->arrayToSql($array);
-        $sql .= " SET " . join(",", $tmp);
-        $sql .= " WHERE id=`{$array['id']}'";
-        echo $sql;
+    function update($array){
+        $sql="UPDATE $this->table ";
+        $tmp=$this->arrayToSql($array);
+        $sql .=" SET ".join(", ",$tmp);
+        $sql .=" WHERE id='{$array['id']}'";
+        //$sql .=" WHERE id='$id'";
+        
+        //echo $sql;
         return $this->pdo->exec($sql);
     }
 
     function insert($array){
+
         $sql="INSERT INTO `{$this->table}` ";
         $keys=array_keys($array);
-        $sql .="(`".join("`,`",$keys). "`)";
-        $sql .=" VALUES ('". join("','",$array)."')";
+        $sql .="(`". join("`,`",$keys). "`)";
+        $sql .=" VALUES ('". join("','",$array). "')";
+        echo $sql;
+        //echo "<hr>";
         return $this->pdo->exec($sql);
+
     }
 
     function del($id){
-        $sql = "DELETE from `$this->table` ";
-        if (is_array($id)){
-            $tmp = $this->arrayToSql($id);
-            $sql .= " where " . implode(" && ", $tmp);
-        }else {
-            $sql .= " where `id`='$id' ";
-        }
+        $sql="DELETE from `$this->table` ";
+                if(is_array($id)){
+                    //多條件
+                    $tmp=$this->arrayToSql($id);
+                    $sql .= " where " . implode(" && ",$tmp);
+                }else{
+                    //單條件
+                    $sql .= " where `id`='$id' ";
+                }
+          
+       //echo $sql;
         return $this->pdo->exec($sql);
     }
 
-    private function arrayTosql($array){
-        $tmp = [];
-        foreach ($array as $key =>$value) {
-            $tmo[] = "`$key`='$value'";
+
+    private function arrayToSql($array){
+        $tmp=[];
+        foreach($array as $key => $value){
+            $tmp[]="`$key`='$value'";
         }
+
         return $tmp;
     }
-
-    function q($sql) {
-        $dsn = "mysql:host=localhost;dbname=db01;charset=utf8";
-        $pdo = new PDO($dsn, 'root','');
-        return $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
-    }
-
-    function to($url) {
-        header("location" . $url);
-    }
 }
-    $Title = new DB('title');
-    $Ad    = new DB('ad');
-    $Mvim  = new DB('mvim');
-    $News  = new DB('news');
-    $Image = new Db('image');
-    $Admin = new DB('admin');
-    $Menu  = new DB('menu');
-    $Total = new DB('total');
-    $Bottom = new DB('bottom');
+
+function q($sql){
+    $dsn="mysql:host=localhost;dbname=db01;charset=utf8";
+    $pdo=new PDO($dsn,'root','');
+    return $pdo->query($sql)->fetchALL(PDO::FETCH_ASSOC);
+}
+
+
+function to($url){
+    header("location:".$url);
+}
+
+
+$Title=new DB('title');
+$Ad=new DB('ad');
+$Mvim=new DB('mvim');
+$News=new DB('news');
+$Image=new DB('image');
+$Admin=new DB('admin');
+$Menu=new DB('menu');
+$Total=new DB('total');
+$Bottom=new DB('bottom');
+
+
+if(!isset($_SESSION['view'])){
+    $_SESSION['view']=1;
+    $total=$Total->find(1);
+    $total['total']++;
+    $Total->save($total);
+
+}
+
 ?>
