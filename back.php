bab72a599f448a02143637dde6fb83dff53c2c24
weir <wei080781@gmail.com1>
2026-01-15 11:42:56 +0800

    修整外部檔案

--- back.php
+++ back.php
@@ -82,31 +82,7 @@
 						</tr>
 					</tbody>
 				</table>
-				<div style="width:99%; height:87%; margin:auto; overflow:auto; border:#666 1px solid;">
-					<p class="t cent botli">網站標題管理</p>
-					<form method="post" target="back" action="?do=tii">
-						<table width="100%">
-							<tbody>
-								<tr class="yel">
-									<td width="45%">網站標題</td>
-									<td width="23%">替代文字</td>
-									<td width="7%">顯示</td>
-									<td width="7%">刪除</td>
-									<td></td>
-								</tr>
-							</tbody>
-						</table>
-						<table style="margin-top:40px; width:70%;">
-							<tbody>
-								<tr>
-									<td width="200px"><input type="button" onclick="op('#cover','#cvr','view.php?do=title')" value="新增網站標題圖片"></td>
-									<td class="cent"><input type="submit" value="修改確定"><input type="reset" value="重置"></td>
-								</tr>
-							</tbody>
-						</table>
-
-					</form>
-				</div>
+				<?php include "./back/title.php";?>
 			</div>
 			<div id="alt" style="position: absolute; width: 350px; min-height: 100px; word-break:break-all; text-align:justify;  background-color: rgb(255, 255, 204); top: 50px; left: 400px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;"></div>
 			<script>
