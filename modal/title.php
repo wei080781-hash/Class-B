5fe909cb983c65290d3a19391a9076216cd35181
weir <wei080781@gmail.com1>
2026-01-16 14:57:30 +0800

    新增modal/title.php修改變數為<?=$do;?>.php

---
+++ modal/title.php
@@ -0,0 +1,12 @@
+<form action="?" method="post">
+    <table>
+        <tr>
+            <td>標題區圖片</td>
+            <td><input type="file" name="img" id=""></td>
+        </tr>
+        <tr>
+            <td>標題區替代文字</td>
+            <td><input type="text" name="alt" id=""></td>
+        </tr>
+    </table>
+</form>
