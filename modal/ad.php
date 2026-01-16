7733c205c1265ed1c1d40b8eeb6614e38d58b6ee
weir <wei080781@gmail.com1>
2026-01-16 14:59:36 +0800

    複製title.php修改名ad.php 動態文字用的

---
+++ modal/ad.php
@@ -0,0 +1,8 @@
+<form action="?" method="post">
+    <table>
+        <tr>
+            <td>動態文字廣告區</td>
+            <td><input type="text" name="alt" id=""></td>
+        </tr>
+    </table>
+</form>
