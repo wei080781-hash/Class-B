e4b1390f876699a58ede93b507eccb2bda5a0ea0
weir <wei080781@gmail.com1>
2026-01-19 17:08:32 +0800

    增加insert_title.php

--- api/insert_title.php
+++ api/insert_title.php
@@ -1,4 +1,15 @@
 <?php
+include_once "db.php";
 
+if(!empty($_FILES['img']['tmp_name'])){
+    move_uploaded_file($_FILES['img']['tmp_name'],"../upload/".$_FILES['img']['name']);
+    $_post['img']=$_FILES['img']['name'];
+}
+
+$POST['sh']=1;
+
+$Title->save($_POST);
+
+to("../back.php?do=title");
 
 ?>
