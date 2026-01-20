bb08c72303954225ffc74a6da23711a24a58b6a8
wei <wei080781@gmail.com>
2026-01-21 04:24:21 +0800

    統一資型大小寫

--- api/insert_title.php
+++ api/insert_title.php
@@ -3,10 +3,10 @@ include_once "db.php";
 
 if(!empty($_FILES['img']['tmp_name'])){
     move_uploaded_file($_FILES['img']['tmp_name'],"../upload/".$_FILES['img']['name']);
-    $_post['img']=$_FILES['img']['name'];
+    $_POST['img']=$_FILES['img']['name'];
 }
 
-$POST['sh']=1;
+$_POST['sh']=1;
 
 $Title->save($_POST);
 
