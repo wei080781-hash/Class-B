9c5b0a5a6f5fb67f8e638a190c8c9e537893faeb
weir <wei080781@gmail.com1>
2026-01-22 16:30:55 +0800

    新增判斷sh_1 or 0  顯示

--- api/insert_title.php
+++ api/insert_title.php
@@ -5,8 +5,13 @@ if(!empty($_FILES['img']['tmp_name'])){
     move_uploaded_file($_FILES['img']['tmp_name'],"../upload/".$_FILES['img']['name']);
     $_POST['img']=$_FILES['img']['name'];
 }
-
-$_POST['sh']=1;
+$countSH=$Title->count(['sh'=>1]);
+$_POST['sh']=($countSH==0)?1:0;
+// if($countSH==0){    
+//     $_POST['sh']=1;
+// }else{
+//     $_POST['sh']=0;
+// }
 
 $Title->save($_POST);
 
