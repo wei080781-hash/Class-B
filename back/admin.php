b1326ca35dc069a7b80a20b88a2bfa62d2b6922f
Wei (wei080781-hash) <wei080781@gmail.com>
2026-02-26 05:06:34 +0800

    新增back_admin .php

---
+++ back/admin.php
@@ -0,0 +1,10 @@
+<div style="width:99%; height:87%; margin:auto; overflow:auto; border:#666 1px solid;">
+    <p class="t cent botli">管理者帳號管理</p>
+    <form method="post" action="./api/edit.php?table=<?=$do;?>">
+
+
+
+
+
+    </form>    
+</div>    
--- back/image.php
+++ back/image.php
@@ -1,15 +1,15 @@
   <div style="width:99%; height:87%; margin:auto; overflow:auto; border:#666 1px solid;">
-        <p class="t cent botli">校園映象資料管理</p>
-        <form method="post" action="./api/edit.php?table=<?=$do;?>">
-          <table width="100%">  
-                <tbody>
-                    <tr class="yel">
-                        <td width="68%">校園映象資料</td>
-                        <td width="7%">顯示</td>
-                        <td width="7%">刪除</td>
-                        <td></td>
-                    </tr>
-                    <?php
+      <p class="t cent botli">校園映象資料管理</p>
+      <form method="post" action="./api/edit.php?table=<?=$do;?>">
+          <table width="100%">
+              <tbody>
+                  <tr class="yel">
+                      <td width="68%">校園映象資料</td>
+                      <td width="7%">顯示</td>
+                      <td width="7%">刪除</td>
+                      <td></td>
+                  </tr>
+                  <?php
                         $total=$Image->count();
                         $div=3;
                         $pages=ceil($total/$div);
@@ -18,29 +18,30 @@
                         $rows=$Image->all(" limit $start,$div");
                         foreach($rows as $row):
                     ?>
-                    <tr>
-                        <td width="68%">
-                        <img src="./upload/<?=$row['img'];?>" style="width:100px;height:68px;">
-                        <input type="hidden" name='id[]' value="<?=$row['id'];?>"> 
-                        </td>
-                        <td width="7%">
-                            <input type="checkbox" name="sh[]" value="<?=$row['id'];?>" <?=($row['sh']==1)?"checked":"";?>>   
-                        </td>
-                        <td width="7%">
-                            <input type="checkbox" name="del[]" value="<?=$row['id'];?>">
-                        </td>
-                        <td>
-                        <input type="button" value="更換圖片" 
-                            onclick="op('#cover','#cvr','./modal/update.php?table=<?=$do;?>&id=<?=$row['id'];?>')">
-                        </td>               
-                    </tr>
-                    <?php
+                  <tr>
+                      <td width="68%">
+                          <img src="./upload/<?=$row['img'];?>" style="width:100px;height:68px;">
+                          <input type="hidden" name='id[]' value="<?=$row['id'];?>">
+                      </td>
+                      <td width="7%">
+                          <input type="checkbox" name="sh[]" value="<?=$row['id'];?>"
+                              <?=($row['sh']==1)?"checked":"";?>>
+                      </td>
+                      <td width="7%">
+                          <input type="checkbox" name="del[]" value="<?=$row['id'];?>">
+                      </td>
+                      <td>
+                          <input type="button" value="更換圖片"
+                              onclick="op('#cover','#cvr','./modal/update.php?table=<?=$do;?>&id=<?=$row['id'];?>')">
+                      </td>
+                  </tr>
+                  <?php
                     endforeach;
                     ?>
-                </tbody> 
-            </table>
-            <div class="cent">
-                <?php 
+              </tbody>
+          </table>
+          <div class="cent">
+              <?php 
                     if($now>1){
                         $prev=$now-1;
                         echo "<a href='?do=$do&p=$prev'> < </a>";
@@ -57,15 +58,18 @@
                     }
 
                 ?>
-            </div>
-
-
-
-
-
-
-
-
-        </from>
-    </div
->
+          </div>
+          <table style="margin-top:40px; width:70%;">
+              <tbody>
+                  <tr>
+                      <td width="200px">
+                          <input type="button" onclick="op('#cover','#cvr','./modal/<?=$do;?>.php?table=<?=$do;?>')"
+                              value="新增校園映象圖片">
+                      </td>
+                      <td class="cent"><input type="submit" value="修改確定"><input type="reset" value="重置">
+                      </td>
+                  </tr>
+              </tbody>
+          </table>
+          </from>
+  </div>
