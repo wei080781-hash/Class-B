860d5f8fed79224d2b69260004fd28e212c592f7
josh <wei080781@gmail.com>
2026-03-06 16:34:30 +0800

    main.php

--- front/main.php
+++ front/main.php
@@ -1,7 +1,7 @@
             <div class="di"
                 style="height:540px; border:#999 1px solid; width:53.2%; margin:2px 0px 0px 0px; float:left; position:relative; left:20px;">
                 <marquee scrolldelay="120" direction="left" style="position:absolute;   width:100%; height:40px;">
-                     <?php
+                    <?php
                         $ads=$Ad->all(['sh'=>1]);
                         foreach($ads as $ad){
                             echo $ad['text']."&nbsp;&nbsp;&nbsp;&nbsp;";
@@ -9,7 +9,7 @@
                         }
                         /* $ad=q("select GROUP_CONCAT(`text` SEPARATOR '&nbsp;&nbsp;&nbsp;&nbsp;') as `texts` from `ad` where `sh`=1 GROUP BY `sh`")[0];
                         echo $ad['texts']; */
-                    ?>    
+                    ?>
                 </marquee>
                 <div style="height:32px; display:block;"></div>
                 <!-- 正中央 -->
@@ -24,17 +24,64 @@
                     <?php
                         $mvims=$Mvim->all(['sh'=>1]);
                         foreach($mvims as $mv){
-                             echo "lin.push('upload/{$mv['img']}');\n";
+                            echo "lin.push('upload/{$mv['img']}');\n";
+                        }
+                    ?>
+
+                    var now = 0;
+                    ww()
+                    if (lin.length > 1) {
+                        setInterval("ww()", 3000);
+                        now = 1;
                     }
-                ?>
-
-                var now = 0;
-                ww()
-                if (lin.length > 1) {
-                    setInterval("ww()", 3000);
-                    now = 1;
+                    function ww() {
+                    $("#mwww").html("<embed loop=true src='" + lin[now] + "' style='width:99%; height:100%;'></embed>")
+                    //$("#mwww").attr("src",lin[now])
+                    now++;
+                    if (now >= lin.length)
+                        now = 0;
                 }
-
                 </script>
+                <div
+                    style="width:95%; padding:2px; height:190px; margin-top:10px; padding:5px 10px 5px 10px; border:#0C3 dashed 3px; position:relative;">
+                    <span class="t botli">最新消息區
+                        <?php
+                            if($News->count(['sh'=>1])>5){
+                                echo "<a href='?do=news' style='float:right;'> More...</a>";
+                            }    
+                        ?>
 
+                    </span>
+                    <?php 
+                        $news=$News->all(['sh'=>1]," limit 5")
+                    ?>
+                    <ul class="ssaa" style="list-style-type:decimal;">
+                        <?php
+                            foreach($news as $n){
+                                echo "<li>";
+                                echo mb_substr($n['text'],0,20);
+                                echo "<div class='all' style='display:none;'>";
+                                echo $n['text'];
+                                echo "</div>";
+                                echo "</li>";
+                             }
+                        ?>
+                    </ul>
+                    <div id="altt"
+                        style="position: absolute; width: 350px; min-height: 100px; background-color: rgb(255, 255, 204); top: 50px; left: 130px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;">
+        
+                    </div>
+                    <script>
+                    $(".ssaa li").hover(
+                        function() {
+                            $("#altt").html("<pre>" + $(this).children(".all").html() + "</pre>")
+                            $("#altt").show()
+                        }
+                    )
+
+
+
+                    </script>    
+                </div>        
             </div>
+        
